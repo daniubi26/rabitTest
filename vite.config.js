3fac67845500ec7bcaed8152b3de2bed174bdda3
daniubi26 <2121950249@qq.com>
2026-03-23 19:09:28 +0800

    添加一级导航渲染

---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+//elementPlus 按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    // ...
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        //1. Element Plus 采用 sass 样式配色系统
+        ElementPlusResolver({importStyle: 'sass'}),
+      ],
+    }),
+  ],
+  resolve: {
+    //实际的路径转换
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  css:{
+    preprocessorOptions:{
+      scss:{
+        //2.自动导入elementPlus样式变量
+        //2.自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+        @use "@/styles/element/index.scss" as *;
+        @use "@/styles/var.scss" as *;
+      `,
+      }
+    }
+  }
+})
