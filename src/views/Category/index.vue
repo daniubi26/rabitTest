b9229e387273724c362915727c977e520318de50
daniubi26 <2121950249@qq.com>
2026-03-26 20:23:43 +0800

    一级列表展示，分类标题渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { getCategoryAPI } from "@/apis/category"
 import { ref } from "vue";
+import { getBannerAPI } from "@/apis/home";
 
+import GoodsItem from "../Home/components/GoodsItem.vue"
 //获取参数
 import { useRoute }from "vue-router"
 
@@ -18,8 +20,18 @@ const getCategory = async () => {
 
 getCategory()
 
+//获取banner
+const bannerList = ref([])
 
+const getBanner=async()=>{
+    const res=await getBannerAPI({
+        distributionSite:'2'
+    })
+    console.log(res)
+    bannerList.value=res.result
+}
 
+getBanner()
 </script>
 
 <template>
@@ -31,6 +43,33 @@ getCategory()
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
+      </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="">
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
       </div>
     </div>
   </div>
@@ -113,6 +152,17 @@ getCategory()
 
   .bread-container {
     padding: 25px 0;
+  }
+}
+
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin:0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
