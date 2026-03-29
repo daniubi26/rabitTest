c5a8785b69c5f1f6637de6696c808722866fe7c4
daniubi26 <2121950249@qq.com>
2026-03-29 11:14:28 +0800

    二级分类-基础列表渲染

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { ref,onMounted } from 'vue'
-import { getCategoryFilterAPI } from '@/apis/category'
+import { getCategoryFilterAPI,getSubCategoryAPI } from '@/apis/category'
 import { useRoute } from 'vue-router'
+import GoodsItem from '../Home/components/GoodsItem.vue'
 
 //获取面包屑导航数据
 const categoryData = ref([])
@@ -13,6 +14,25 @@ const getCategoryData=async()=>{
 
 onMounted(()=>{
     getCategoryData()
+})
+
+//获取基础列表数据渲染
+const goodsList=ref([])
+const reqData=ref({
+    categoryId:route.params.id,
+    page:1,
+    pageSize:20,
+    sortField:'publishTime'
+})
+const getGoodsList=async()=>{
+    const res=await getSubCategoryAPI(reqData.value)
+    console.log(res);
+    goodsList.value=res.result.items
+}
+
+
+onMounted(()=>{
+    getGoodsList()
 })
 
 </script>
@@ -36,6 +56,7 @@ onMounted(()=>{
       </el-tabs>
       <div class="body">
          <!-- 商品列表-->
+        <GoodsItem v-for="goods in goodsList" :goods="goods" :key="goods.id"/>
       </div>
     </div>
   </div>
