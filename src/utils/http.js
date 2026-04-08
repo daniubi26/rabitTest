f8fa82ee3ae9af08d9384e9ce224cc44b039be4d
daniubi26 <2121950249@qq.com>
2026-04-08 17:01:48 +0800

    登录提示

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,7 @@
 //axios基础的封装
 import axios from "axios";
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
 
 const httpInstance = axios.create({
     baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
@@ -24,6 +26,11 @@ httpInstance.interceptors.response.use(res => {
 
     return res.data
 }, err => {
+    //统一错误提示
+    ElMessage({
+        type:'warning',
+        message:err.response.data.message
+    })
     return Promise.reject(err)
 })
 
