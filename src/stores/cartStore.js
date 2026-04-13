a1c39a7674d72d990635cc8596574c5b8c42e8e5
daniubi26 <2121950249@qq.com>
2026-04-13 17:14:06 +0800

    购物车图标模板

---
+++ src/stores/cartStore.js
@@ -0,0 +1,31 @@
+//封装购物车模块
+
+import { ElStep } from 'element-plus'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useCartStore=defineStore('cart',()=>{ 
+    //1.定义state -cartList
+    const cartList = ref([])
+
+    //2.定义action -addCart
+    const addCart=(goods)=>{
+        //添加购物车操作
+
+        //已添加过 -count +1 
+
+        //没有添加过，直接push
+        const item=cartList.value.find((item)=>goods.skuId===item.skuId)
+        if(item){
+            item.count++
+        }else{
+            cartList.value.push(goods)
+        }
+    }
+    return{
+        cartList,
+        addCart
+    }
+},{
+    persist:true
+})
--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -3,6 +3,7 @@ import { getCategoryAPI } from '@/apis/layout.js'
 import { onMounted } from 'vue'
 import { ref } from 'vue'
 import { useCategoryStore } from '@/stores/category';
+import HeaderCart from './HeaderCart.vue';
 const categoryStore = useCategoryStore()
 </script>
 
@@ -22,7 +23,7 @@ const categoryStore = useCategoryStore()
         <input type="text" placeholder="搜一搜">
       </div>
       <!-- 头部购物车 -->
-      
+      <HeaderCart/>
     </div>
   </header>
 </template>
